175514c4ec1ac11f191277792efe314385cbeabe
z7md <z7mdd90@gmail.com>
2024-04-01 23:00:10 +0300

    a lot of changing

--- backend/models/subModel.js
+++ backend/models/subModel.js
@@ -23,6 +23,18 @@ var SubSchema = new mongoose.Schema({
         type:Number,
         required:true
     },
+    mealsLeft:{
+    type:Number,
+    required:true
+    },
+    mealTime:{
+        type:Array,
+        required:true
+    },
+    expirationDate:{
+        type:String,
+        required:true
+    }
   }, {timestamps: true});
   
 export const Sub =mongoose.model('Sub', SubSchema);
--- backend/routes/subRoute.js
+++ backend/routes/subRoute.js
@@ -1,103 +1,103 @@
 import express from "express";
 import { Sub } from "../models/subModel.js";
+import moment from "moment";
 
-const router=express.Router();
+const router = express.Router();
 
-router.post("/",async (request,response)=>{
-    try{
-        if(
-            !request.body.subname ||
-            !request.body.phone ||
-            !request.body.meals ||
-            !request.body.carb ||
-            !request.body.protein 
-        ){
-            return response.status(400).send({
-                message: "Please send all information",
-            });
-        }
-        const newSub={
-            subname :request.body.subname, 
-            phone :request.body.phone, 
-            meals :request.body.meals,
-            carb :request.body.carb,
-            protein :request.body.protein 
-        };
-        const sub = await Sub.create(newSub);
-        return response.status(201).send(sub)
-    }catch(error){
-    console.log(error.message);
-    response.status(500).send({message:error.message});
+router.post("/", async (request, response) => {
+  try {
+    if (
+      !request.body.subname ||
+      !request.body.phone ||
+      !request.body.meals ||
+      !request.body.carb ||
+      !request.body.protein
+    ) {
+      return response.status(400).send({
+        message: "Please send all information",
+      });
     }
+    const newSub = {
+      subname: request.body.subname,
+      phone: request.body.phone,
+      meals: request.body.meals,
+      carb: request.body.carb,
+      protein: request.body.protein,
+      mealsLeft: request.body.meals * 26,
+      mealTime: new Array(),
+      expirationDate: moment().add(60, 'days').calendar(),
+    };
+    const sub = await Sub.create(newSub);
+    return response.status(201).send(sub);
+  } catch (error) {
+    console.log(error.message);
+    response.status(500).send({ message: error.message });
+  }
+});
+
+router.get("/", async (request, response) => {
+  try {
+    const subs = await Sub.find({});
+    return response.status(200).json({
+      count: subs.length,
+      data: subs,
     });
-    
-    router.get("/",async(request,response)=>{
-        try{
-            const subs = await Sub.find({});
-            return response.status(200).json({
-                count:subs.length,
-                data:subs
-            })
-        }catch(error){
-            console.log(error.message);
-            response.status(500).send({message:error.message});
-        }
-    
-    });
-    
-    router.get("/:id",async(request,response)=>{
-        try{
-            const {id}=request.params;
-    
-            const sub = await Sub.findById(id);
-            return response.status(200).json(sub)
-        }catch(error){
-            console.log(error.message);
-            response.status(500).send({message:error.message});
-        }
-    
-    });
-    router.put("/:id",async(request,response)=>{
-        try {
-            if(
-                !request.body.subname ||
-                !request.body.phone ||
-                !request.body.meals ||
-                !request.body.carb ||
-                !request.body.protein 
-            ){
-                return response.status(400).send({
-                    message: "Please send all information",
-                });
-            }
-            const {id}=request.params;
-            const result =await Sub.findByIdAndUpdate(id,request.body);
-            if(!result){
-                return response.status(404).json({message:"Sub not found"})
-            }else {
-                return response.status(200).json({message:"Sub Updated!"})
-            }
-            
-        } catch (error) {
-            console.log(error.message);
-            response.status(500).send({message:error.message})
-        }
-    })
-    
-    router.delete("/:id",async(request,response)=>{
-        try {
-            const {id}=request.params;
-            const result = await Sub.findByIdAndDelete(id);
-    
-            if(!result){
-                return response.status(404).send({message:"Sub not found"})
-            }else {
-                return response.status(404).send({message:"Sub deleted!"})
-            }
-    
-        } catch (error) {
-            console.log(error.message);
-            response.status(505).send({message:error.message});
-        }
-    });
-    export default router;
+  } catch (error) {
+    console.log(error.message);
+    response.status(500).send({ message: error.message });
+  }
+});
+
+router.get("/:id", async (request, response) => {
+  try {
+    const { id } = request.params;
+
+    const sub = await Sub.findById(id);
+    return response.status(200).json(sub);
+  } catch (error) {
+    console.log(error.message);
+    response.status(500).send({ message: error.message });
+  }
+});
+router.put("/:id", async (request, response) => {
+  try {
+    if (
+      !request.body.subname ||
+      !request.body.phone ||
+      !request.body.meals ||
+      !request.body.carb ||
+      !request.body.protein
+    ) {
+      return response.status(400).send({
+        message: "Please send all information",
+      });
+    }
+    const { id } = request.params;
+    const result = await Sub.findByIdAndUpdate(id, request.body);
+    if (!result) {
+      return response.status(404).json({ message: "Sub not found" });
+    } else {
+      return response.status(200).json({ message: "Sub Updated!" });
+    }
+  } catch (error) {
+    console.log(error.message);
+    response.status(500).send({ message: error.message });
+  }
+});
+
+router.delete("/:id", async (request, response) => {
+  try {
+    const { id } = request.params;
+    const result = await Sub.findByIdAndDelete(id);
+
+    if (!result) {
+      return response.status(404).send({ message: "Sub not found" });
+    } else {
+      return response.status(201).send({ message: "Sub deleted!" });
+    }
+  } catch (error) {
+    console.log(error.message);
+    response.status(505).send({ message: error.message });
+  }
+});
+export default router;
